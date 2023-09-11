23e5e5b385b9c2bc0182eb68780ce999c879ca6a
Mathias Daitx <mathiasdaitx@rede.ulbra.br>
2023-09-11 20:40:15 -0300

    entrega ap1

--- view/home.php
+++ view/home.php
@@ -18,7 +18,7 @@
     <h3>Mathias Daitx Santos de Oliveira</h3>
 </header>
 
-<div id="carouselExampleCaptions" class="carousel slide">
+<div id="carouselExampleCaptions" class="carousel slide vh-90">
   <div class="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
